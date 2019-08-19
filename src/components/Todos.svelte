e1ba3e9030df29c765a1ffe62683b2f82a334f8f
Denis Bogush <dmbogush91@gmail.com>
2019-08-19 08:57:30 +0300

    Add $ for complited tasks

--- src/components/Todos.svelte
+++ src/components/Todos.svelte
@@ -3,8 +3,6 @@
 
   let todoItems = [];
   let newTodo = "";
-  let notComplitedItems = [];
-  let complitedItems = [];
 
   const addTodo = () => {
     newTodo = newTodo.trim();
@@ -20,33 +18,21 @@
 
     todoItems = [...todoItems, todo];
     newTodo = "";
-
-    getComplitedItems();
   };
 
   const toggleDone = id => {
     const index = todoItems.findIndex(item => item.id === Number(id));
     todoItems[index].checked = !todoItems[index].checked;
-
-    getComplitedItems();
   };
 
   const deleteTodo = id => {
     todoItems = todoItems.filter(item => item.id !== Number(id));
-
-    getComplitedItems();
-  };
-
-  const getComplitedItems = () => {
-    notComplitedItems = todoItems.filter(item => !item.checked);
-    complitedItems = todoItems.filter(item => item.checked);
   };
 </script>
 
 <div>
   <TodosList
-    {notComplitedItems}
-    {complitedItems}
+    {todoItems}
     {addTodo}
     {toggleDone}
     {deleteTodo}
